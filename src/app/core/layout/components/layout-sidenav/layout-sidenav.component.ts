e4b527cd33352c52f9b749a8d3cc0f02f03fdecd
Mateusz Kaniecki <mateusz.kaniecki@iteo.com>
2023-03-25 21:23:16 +0100

    Create sidenav application navigator

---
+++ src/app/core/layout/components/layout-sidenav/layout-sidenav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-sidenav',
+  templateUrl: './layout-sidenav.component.html',
+  styleUrls: ['./layout-sidenav.component.scss'],
+})
+export class LayoutSidenavComponent {}
--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutSidenavComponent } from './components/layout-sidenav/layout-sidenav.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, LayoutSidenavComponent],
   exports: [LayoutComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatSidenavModule, MatButtonModule, MatIconModule],
 })
 export class LayoutModule {}
